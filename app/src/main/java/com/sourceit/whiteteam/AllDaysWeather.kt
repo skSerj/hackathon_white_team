4762dca0111fc4c5771f269b4bd05b98492c5159
kuzavkov <serg1992kuzavkov@gmail.com>
2020-06-30 18:54:20 +0300

    add Fragments and recycler View

--- app/src/main/java/com/sourceit/whiteteam/AllDaysWeather.kt
+++ app/src/main/java/com/sourceit/whiteteam/AllDaysWeather.kt
@@ -5,55 +5,50 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import com.sourceit.whiteteam.adapter.WeatherAdapter
+import com.sourceit.whiteteam.network.ApiService
+import com.sourceit.whiteteam.network.RequestModel
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [AllDaysWeather.newInstance] factory method to
- * create an instance of this fragment.
- */
 class AllDaysWeather : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+    private lateinit var disposable: Disposable
+    private lateinit var weatherAdapter: WeatherAdapter
+    private var listOfWeather: MutableList<RequestModel> = ArrayList()
+
+    companion object {
+        fun newInstance() = AllDaysWeather()
+
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_all_days_weather, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment AllDaysWeather.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            AllDaysWeather().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        disposable = ApiService.getFiveDayWeather()
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe({
+                listOfWeather.add(it)
+                showInfo(it)
+            }, {
+                Toast.makeText(view.context, "error", Toast.LENGTH_SHORT).show()
+                it.printStackTrace()
+            })
+    }
+
+    private fun showInfo(weatherInfo: RequestModel) {
+        weatherAdapter.update(weatherInfo)
     }
 }
