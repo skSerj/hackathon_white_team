d8d143f3608819e80005d6e801f11083aeae53fe
kuzavkov <serg1992kuzavkov@gmail.com>
2020-06-30 17:41:10 +0300

    add model

---
+++ app/src/main/java/com/sourceit/whiteteam/network/RequestModel.kt
@@ -0,0 +1,42 @@
+package com.sourceit.whiteteam.network
+
+import com.google.gson.annotations.SerializedName
+
+data class RequestModel(
+    @SerializedName("List")
+    var list: List<WeatherInfo>
+)
+
+data class WeatherInfo(
+    var main: Main,
+    var weather: List<Weather>,
+    var clouds: Clouds,
+    var wind: Wind,
+    var dtTxt: String
+)
+
+data class Main(
+    var temp: Float,
+    var feelsLike: Float,
+    var tempMin: Float,
+    var tempMax: Float,
+    var pressure: Int,
+    var humidity: Int
+)
+
+data class Wind(
+    var speed: Float,
+    var deg: Int
+)
+
+data class Clouds(var all: Int)
+
+data class Weather(
+    var id: Int,
+    var main: String,
+    var description: String,
+    var icon: String
+)
+
+
+
